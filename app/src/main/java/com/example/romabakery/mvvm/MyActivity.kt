53f2f3b311e2177d98bd01b99eb53d3eda128e0d
artazidele <77989249+artazidele@users.noreply.github.com>
2022-02-10 09:11:04 +0200

    refreshallergenlist function

--- app/src/main/java/com/example/romabakery/mvvm/MyActivity.kt
+++ app/src/main/java/com/example/romabakery/mvvm/MyActivity.kt
@@ -17,6 +17,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.romabakery.databinding.ActivityMyBinding
+import com.example.romabakery.model.Allergen
 import com.example.romabakery.viewmodel.AllergenViewModel
 import com.example.romabakery.viewmodel.NetworkViewModel
 
@@ -43,49 +44,29 @@ class MyActivity : AppCompatActivity() {
 
 
         if (NetworkViewModel().checkConnection(this) == true) {
-//            allergenViewModel.getLiveAllergens() { result ->
-//                if (result?.value != null) {
-//                    binding.allAllergenRecyclerView.adapter = MySecondAdapter()
-//                    binding.allAllergenRecyclerView.visibility = View.VISIBLE
-//                    Log.d(ContentValues.TAG, "NOT EMPTY")
-//                    allergenViewModel.allergens.observe(this, Observer { allergens ->
-//                        // Update the cached copy of the words in the adapter.
-//                        allergens?.let { MySecondAdapter().submitList(allergens) }
-//                        Log.d(ContentValues.TAG, allergens.size.toString())
-//
-//                    })
+            refreshAllergenList()
+//            allergenViewModel.getAllergens() {
+//                if (it?.isNotEmpty() == true) {
+//                    refreshAllergenList(it)
+////                    binding.allAllergenRecyclerView.adapter = MyAdapter(it!!)
+////                    binding.allAllergenRecyclerView.visibility = View.VISIBLE
+//                } else {
+//                    Log.d(ContentValues.TAG, "EMPTY")
 //                }
-//
 //            }
+        }
+    }
 
-
-
-            allergenViewModel.getAllergens() {
-                if (it?.isNotEmpty() == true) {
+    private fun refreshAllergenList() {
+        allergenViewModel.getAllergens() {
+            if (it?.isNotEmpty() == true) {
                     binding.allAllergenRecyclerView.adapter = MyAdapter(it!!)
                     binding.allAllergenRecyclerView.visibility = View.VISIBLE
-                    Log.d(ContentValues.TAG, "NOT EMPTY")
-                } else {
-                    Log.d(ContentValues.TAG, "EMPTY")
-                }
+                Log.d(ContentValues.TAG, "NOT EMPTY")
+            } else {
+                Log.d(ContentValues.TAG, "EMPTY")
             }
-
-
-
-
-
-
-////            val data = allergenViewModel.getAllergens()
-////            binding.allAllergenRecyclerView.adapter = AllergenAdapter(data)//()//(data)
-////            binding.allAllergenRecyclerView.visibility = View.VISIBLE
-//
-//
-////            showAllergens() //allergenViewModel.getAllAllergens()
-////            allergenViewModel.getAllAllergens()
-////            binding.allAllergenRecyclerView.visibility = View.VISIBLE
         }
-//
-//        setHasOptionsMenu(true)
     }
 
 //    private fun itemRemoved(int: Int) {
