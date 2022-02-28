c48a2aacd6484a34460e730b8672186e946fb9fe
artazidele <77989249+artazidele@users.noreply.github.com>
2022-02-28 21:14:09 +0200

    add viewmodelscope

--- app/src/main/java/com/example/romabakery/bekereja/viewmodels/AllergenDataViewModel.kt
+++ app/src/main/java/com/example/romabakery/bekereja/viewmodels/AllergenDataViewModel.kt
@@ -3,107 +3,121 @@ package com.example.romabakery.bekereja.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.romabakery.bekereja.models.allergens.AllergenDataClass
 import com.example.romabakery.bekereja.models.allergens.AllergenDatabase
 import com.example.romabakery.bekereja.models.items.ItemDataClass
 import com.example.romabakery.bekereja.models.items.ItemDatabase
 import com.google.firebase.firestore.ktx.toObject
+import kotlinx.coroutines.launch
 
 class AllergenDataViewModel : ViewModel() {
     private val _status = MutableLiveData<NetworkDataStatus>()
     val status: LiveData<NetworkDataStatus> = _status
 
     fun addNewAllergen(allergen: AllergenDataClass, onResult: (Boolean) -> Unit) {
-        _status.value = NetworkDataStatus.LOADING
-        AllergenDatabase().addAllergen(allergen)
-            .addOnSuccessListener { documents ->
-                _status.value = NetworkDataStatus.DONE
-                onResult(true)
-            }
-            .addOnFailureListener {
-                _status.value = NetworkDataStatus.ERROR
-                onResult(false)
-            }
+        viewModelScope.launch {
+            _status.value = NetworkDataStatus.LOADING
+            AllergenDatabase().addAllergen(allergen)
+                .addOnSuccessListener { documents ->
+                    _status.value = NetworkDataStatus.DONE
+                    onResult(true)
+                }
+                .addOnFailureListener {
+                    _status.value = NetworkDataStatus.ERROR
+                    onResult(false)
+                }
+        }
     }
 
     fun getOneAllergen(id: String, onResult: (AllergenDataClass?) -> Unit) {
-        _status.value = NetworkDataStatus.LOADING
-        AllergenDatabase().getAllergen(id)
-            .addOnSuccessListener { document ->
-                _status.value = NetworkDataStatus.DONE
-                val allergen = document.toObject<AllergenDataClass>()
-                onResult(allergen)
-            }
-            .addOnFailureListener {
-                _status.value = NetworkDataStatus.ERROR
-                onResult(null)
-            }
+        viewModelScope.launch {
+            _status.value = NetworkDataStatus.LOADING
+            AllergenDatabase().getAllergen(id)
+                .addOnSuccessListener { document ->
+                    _status.value = NetworkDataStatus.DONE
+                    val allergen = document.toObject<AllergenDataClass>()
+                    onResult(allergen)
+                }
+                .addOnFailureListener {
+                    _status.value = NetworkDataStatus.ERROR
+                    onResult(null)
+                }
+        }
     }
 
     fun getAllergenItems(id: String, onResult: (ArrayList<ItemDataClass>?) -> Unit) {
-        _status.value = NetworkDataStatus.LOADING
-        var itemListWithAllergen = ArrayList<ItemDataClass>()
-        ItemDatabase().getItems()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    val oneItem = document.toObject<ItemDataClass>()
-                    for (allergen in oneItem.containsAllergens) {
-                        if (allergen == id) {
-                            itemListWithAllergen.add(oneItem)
-                            break
+        viewModelScope.launch {
+            _status.value = NetworkDataStatus.LOADING
+            var itemListWithAllergen = ArrayList<ItemDataClass>()
+            ItemDatabase().getItems()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        val oneItem = document.toObject<ItemDataClass>()
+                        for (allergen in oneItem.containsAllergens) {
+                            if (allergen == id) {
+                                itemListWithAllergen.add(oneItem)
+                                break
+                            }
                         }
                     }
+                    _status.value = NetworkDataStatus.DONE
+                    onResult(itemListWithAllergen)
                 }
-                _status.value = NetworkDataStatus.DONE
-                onResult(itemListWithAllergen)
-            }
-            .addOnFailureListener {
-                _status.value = NetworkDataStatus.ERROR
-                onResult(null)
-            }
+                .addOnFailureListener {
+                    _status.value = NetworkDataStatus.ERROR
+                    onResult(null)
+                }
+        }
     }
 
     fun deleteOneAllergen(id: String, onResult: (Boolean) -> Unit) {
-        _status.value = NetworkDataStatus.LOADING
-        AllergenDatabase().deleteAllergen(id)
-            .addOnSuccessListener {
-                _status.value = NetworkDataStatus.DONE
-                onResult(true)
-            }
-            .addOnFailureListener {
-                _status.value = NetworkDataStatus.ERROR
-                onResult(false)
-            }
+        viewModelScope.launch {
+            _status.value = NetworkDataStatus.LOADING
+            AllergenDatabase().deleteAllergen(id)
+                .addOnSuccessListener {
+                    _status.value = NetworkDataStatus.DONE
+                    onResult(true)
+                }
+                .addOnFailureListener {
+                    _status.value = NetworkDataStatus.ERROR
+                    onResult(false)
+                }
+        }
     }
 
     fun updateOneAllergen(allergen: AllergenDataClass, onResult: (Boolean) -> Unit) {
-        _status.value = NetworkDataStatus.LOADING
-        AllergenDatabase().updateAllergen(allergen)
-            .addOnSuccessListener {
-                _status.value = NetworkDataStatus.DONE
-                onResult(true)
-            }
-            .addOnFailureListener {
-                _status.value = NetworkDataStatus.ERROR
-                onResult(false)
-            }
+        viewModelScope.launch {
+            _status.value = NetworkDataStatus.LOADING
+            AllergenDatabase().updateAllergen(allergen)
+                .addOnSuccessListener {
+                    _status.value = NetworkDataStatus.DONE
+                    onResult(true)
+                }
+                .addOnFailureListener {
+                    _status.value = NetworkDataStatus.ERROR
+                    onResult(false)
+                }
+        }
     }
 
     fun getAllAllergens(onResult: (ArrayList<AllergenDataClass>?) -> Unit) {
-        var allergenList = ArrayList<AllergenDataClass>()
-        _status.value = NetworkDataStatus.LOADING
-        AllergenDatabase().getAllergens()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    val allergen = document.toObject<AllergenDataClass>()
-                    allergenList.add(allergen)
+        viewModelScope.launch {
+            var allergenList = ArrayList<AllergenDataClass>()
+            _status.value = NetworkDataStatus.LOADING
+            AllergenDatabase().getAllergens()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        val allergen = document.toObject<AllergenDataClass>()
+                        allergenList.add(allergen)
+                    }
+                    _status.value = NetworkDataStatus.DONE
+                    onResult(allergenList)
                 }
-                _status.value = NetworkDataStatus.DONE
-                onResult(allergenList)
-            }
-            .addOnFailureListener {
-                _status.value = NetworkDataStatus.ERROR
-                onResult(null)
-            }
+                .addOnFailureListener {
+                    _status.value = NetworkDataStatus.ERROR
+                    onResult(null)
+                }
+        }
     }
 }
--- app/src/main/java/com/example/romabakery/bekereja/viewmodels/ItemDataViewModel.kt
+++ app/src/main/java/com/example/romabakery/bekereja/viewmodels/ItemDataViewModel.kt
@@ -3,42 +3,48 @@ package com.example.romabakery.bekereja.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.romabakery.bekereja.models.allergens.AllergenDataClass
 import com.example.romabakery.bekereja.models.allergens.AllergenDatabase
 import com.example.romabakery.bekereja.models.items.ItemDataClass
 import com.example.romabakery.bekereja.models.items.ItemDatabase
 import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.toObject
+import kotlinx.coroutines.launch
 
 class ItemDataViewModel : ViewModel() {
     private val _status = MutableLiveData<NetworkDataStatus>()
     val status: LiveData<NetworkDataStatus> = _status
 
     fun addNewItem(item: ItemDataClass, onResult: (Boolean) -> Unit) {
-        _status.value = NetworkDataStatus.LOADING
-        ItemDatabase().addItem(item)
-            .addOnSuccessListener { documents ->
-                _status.value = NetworkDataStatus.DONE
-                onResult(true)
-            }
-            .addOnFailureListener {
-                _status.value = NetworkDataStatus.ERROR
-                onResult(false)
-            }
+        viewModelScope.launch {
+            _status.value = NetworkDataStatus.LOADING
+            ItemDatabase().addItem(item)
+                .addOnSuccessListener { documents ->
+                    _status.value = NetworkDataStatus.DONE
+                    onResult(true)
+                }
+                .addOnFailureListener {
+                    _status.value = NetworkDataStatus.ERROR
+                    onResult(false)
+                }
+        }
     }
 
     fun getOneItem(id: String, onResult: (ItemDataClass?) -> Unit) {
-        _status.value = NetworkDataStatus.LOADING
-        ItemDatabase().getItem(id)
-            .addOnSuccessListener { document ->
-                _status.value = NetworkDataStatus.DONE
-                val item = document.toObject<ItemDataClass>()
-                onResult(item)
-            }
-            .addOnFailureListener {
-                _status.value = NetworkDataStatus.ERROR
-                onResult(null)
-            }
+        viewModelScope.launch {
+            _status.value = NetworkDataStatus.LOADING
+            ItemDatabase().getItem(id)
+                .addOnSuccessListener { document ->
+                    _status.value = NetworkDataStatus.DONE
+                    val item = document.toObject<ItemDataClass>()
+                    onResult(item)
+                }
+                .addOnFailureListener {
+                    _status.value = NetworkDataStatus.ERROR
+                    onResult(null)
+                }
+        }
     }
 
     fun getItemAllergens(
@@ -46,52 +52,58 @@ class ItemDataViewModel : ViewModel() {
         id: String,
         onResult: (ArrayList<AllergenDataClass>?) -> Unit
     ) {
-        _status.value = NetworkDataStatus.LOADING
-        var itemAllergenList = ArrayList<AllergenDataClass>()
-        AllergenDatabase().getAllergens()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    val oneAllergen = document.toObject<AllergenDataClass>()
-                    for (allergen in item.containsAllergens) {
-                        if (allergen == oneAllergen.id) {
-                            itemAllergenList.add(oneAllergen)
-                            break
+        viewModelScope.launch {
+            _status.value = NetworkDataStatus.LOADING
+            var itemAllergenList = ArrayList<AllergenDataClass>()
+            AllergenDatabase().getAllergens()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        val oneAllergen = document.toObject<AllergenDataClass>()
+                        for (allergen in item.containsAllergens) {
+                            if (allergen == oneAllergen.id) {
+                                itemAllergenList.add(oneAllergen)
+                                break
+                            }
                         }
                     }
+                    _status.value = NetworkDataStatus.DONE
+                    onResult(itemAllergenList)
                 }
-                _status.value = NetworkDataStatus.DONE
-                onResult(itemAllergenList)
-            }
-            .addOnFailureListener {
-                _status.value = NetworkDataStatus.ERROR
-                onResult(null)
-            }
+                .addOnFailureListener {
+                    _status.value = NetworkDataStatus.ERROR
+                    onResult(null)
+                }
+        }
     }
 
     fun deleteOneItem(id: String, onResult: (Boolean) -> Unit) {
-        _status.value = NetworkDataStatus.LOADING
-        ItemDatabase().deleteItem(id)
-            .addOnSuccessListener {
-                _status.value = NetworkDataStatus.DONE
-                onResult(true)
-            }
-            .addOnFailureListener {
-                _status.value = NetworkDataStatus.ERROR
-                onResult(false)
-            }
+        viewModelScope.launch {
+            _status.value = NetworkDataStatus.LOADING
+            ItemDatabase().deleteItem(id)
+                .addOnSuccessListener {
+                    _status.value = NetworkDataStatus.DONE
+                    onResult(true)
+                }
+                .addOnFailureListener {
+                    _status.value = NetworkDataStatus.ERROR
+                    onResult(false)
+                }
+        }
     }
 
     fun updateOneItem(item: ItemDataClass, onResult: (Boolean) -> Unit) {
-        _status.value = NetworkDataStatus.LOADING
-        ItemDatabase().updateItem(item)
-            .addOnSuccessListener {
-                _status.value = NetworkDataStatus.DONE
-                onResult(true)
-            }
-            .addOnFailureListener {
-                _status.value = NetworkDataStatus.ERROR
-                onResult(false)
-            }
+        viewModelScope.launch {
+            _status.value = NetworkDataStatus.LOADING
+            ItemDatabase().updateItem(item)
+                .addOnSuccessListener {
+                    _status.value = NetworkDataStatus.DONE
+                    onResult(true)
+                }
+                .addOnFailureListener {
+                    _status.value = NetworkDataStatus.ERROR
+                    onResult(false)
+                }
+        }
     }
 
     fun getAllItems(
@@ -106,28 +118,30 @@ class ItemDataViewModel : ViewModel() {
         notInProduction: Boolean,
         onResult: (ArrayList<ItemDataClass>?) -> Unit
     ) {
-        _status.value = NetworkDataStatus.LOADING
-        ItemDatabase().getItems()
-            .addOnSuccessListener { documents ->
-                var itemList = filterItems(
-                    bun,
-                    cake,
-                    cookies,
-                    forVegans,
-                    forVegetarians,
-                    withoutFlour,
-                    withoutLactose,
-                    notContainsAllergens,
-                    notInProduction,
-                    documents
-                )
-                _status.value = NetworkDataStatus.DONE
-                onResult(itemList)
-            }
-            .addOnFailureListener {
-                _status.value = NetworkDataStatus.ERROR
-                onResult(null)
-            }
+        viewModelScope.launch {
+            _status.value = NetworkDataStatus.LOADING
+            ItemDatabase().getItems()
+                .addOnSuccessListener { documents ->
+                    var itemList = filterItems(
+                        bun,
+                        cake,
+                        cookies,
+                        forVegans,
+                        forVegetarians,
+                        withoutFlour,
+                        withoutLactose,
+                        notContainsAllergens,
+                        notInProduction,
+                        documents
+                    )
+                    _status.value = NetworkDataStatus.DONE
+                    onResult(itemList)
+                }
+                .addOnFailureListener {
+                    _status.value = NetworkDataStatus.ERROR
+                    onResult(null)
+                }
+        }
     }
 
     fun filterItems(
@@ -154,7 +168,7 @@ class ItemDataViewModel : ViewModel() {
                 || (oneItem.forVegans == false && forVegans == true)
                 || (oneItem.forVegetarians == false && forVegetarians == true)
                 || (oneItem.withoutFlour == false && withoutFlour == true)
-                || (oneItem.withoutLactose == false && withoutFlour == true)
+                || (oneItem.withoutLactose == false && withoutLactose == true)
                   && oneItem.notInProduction == notInProduction) {
                 isInList = false
             }
